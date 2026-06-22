b83f11e9e29f38c431333251f41de4dd5696b12a
Anton <Antonioum80@gmail.com>
2026-06-22 20:25:22 +0300

    Add memory monitoring script

--- check_memory.sh
+++ check_memory.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-
+echo ""
+echo "======= MEMORY CHECK ========="
+echo ""
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 tot_mem=$(free -h | tail -2 | head -1 | awk '{print $2}') 
 us_mem=$(free -h | tail -2 | head -1 | awk '{print $3}')
 fr_mem=$(free -h | tail -2 | head -1 | awk '{print $4}')
-Alrm=80
-mem_line=$(free -h | grep 'Mem')
-echo $mem_line
-echo "Total memory - $tot_mem"
-echo "Used memory - $us_mem"
-echo "Free memory - $fr_mem"
+THRESHOLD=80
+used_perc=$(free | grep Mem | awk '{print int($3/$2 * 100)}')
+echo "Total memory : $tot_mem"
+echo "Used memory  : $us_mem"
+echo "Free memory  : $fr_mem"
+echo "Usage        : $used_perc%"
+echo ""
+if [ "$used_perc" -ge "$THRESHOLD" ]
+then
+	echo -e "${RED} WARNING:${NC} MEMORY USAGE IS "$used_perc" % "
+else
+	echo -e "${GREEN} OK,${NC} MEMORY USAGE IS "$used_perc"% "
+fi
+echo ""
