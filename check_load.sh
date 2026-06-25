6b21adc4d6a1df08990d0bb8df5917653187d818
Anton <Antonioum80@gmail.com>
2026-06-25 17:11:06 +0300

    Added new script check_load.sh

---
+++ check_load.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo ""
+echo "============ UTILIZATION CHECK ============"
+echo ""
+UPTIME=$(uptime | awk '{print $1}')
+one_min=$(uptime | awk '{print $8}'| tr -d ",")
+five_min=$(uptime | awk '{print $9}'| tr -d ",")
+fifteen_min=$(uptime | awk '{print $10}'| tr -d ",")
+THRESHOLD=4.0
+echo "Current uptime: $UPTIME"
+echo "--------------------------"
+echo "Load average:"
+
+echo "1 min: $one_min"
+echo "5 min: $five_min"
+echo "15 min: $fifteen_min"
+if awk "BEGIN {exit !($one_min > $THRESHOLD)}"
+then
+    echo "WARNING"
+else
+    echo "OK"
+fi
